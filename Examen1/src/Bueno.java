ee5d52196eb864ca0c1b7395b34fe7d41a7b839f
Gustavo Ferrufino <gustavo.fdlf@gmail.com>
2014-02-10 14:31:30 -0600

    Directory for first project exam

---
+++ Examen1/src/Bueno.java
@@ -0,0 +1,23 @@
+/**
+ * Clase Planeta
+ *
+ * @author Gustavo Ferrufino
+ * @version 1.00 2008/6/13
+ */
+import java.awt.Image;
+
+public class Bueno extends Base{
+
+	/**
+	 * Metodo constructor que hereda los atributos de la clase <code>Figura</code>.
+	 * @param posX es la <code>posiscion en x</code> del objeto planeta.
+	 * @param posY es el <code>posiscion en y</code> del objeto planeta.
+	 * @param image es la <code>imagen</code> del objeto planeta.
+	 */
+	public Bueno(int posX,int posY,Image image){
+		super(posX,posY,image);	
+           
+	
+        }
+        
+}
